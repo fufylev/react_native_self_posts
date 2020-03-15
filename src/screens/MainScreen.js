b959fdb8064fd315696206d732ac231378e695c7
Andrey <35691656+fufylev@users.noreply.github.com>
2020-03-15 15:57:16 +0300

    SQL DB was applied and checked for all GET, ADD, UPDATE & DELETE methods
    SQL DB was applied and checked for all GET, ADD, UPDATE & DELETE methods

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,11 +1,12 @@
 import React, { useEffect } from 'react';
+import { View, StyleSheet, ActivityIndicator } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import { useDispatch, useSelector } from 'react-redux';
-
 
 import AppHeaderIcon from '../components/AppHeaderIcon';
 import { PostList } from '../components/PostList';
 import { loadPosts } from '../store/actions/post';
+import { THEME } from '../theme';
 
 const MainScreen = ({ navigation }) => {
     const openPostHandler = post => {
@@ -19,10 +20,19 @@ const MainScreen = ({ navigation }) => {
     const dispatch = useDispatch();
 
     useEffect(() => {
-        dispatch(loadPosts())
+        dispatch(loadPosts());
     }, [dispatch]);
 
-    const allPosts = useSelector(state => state.post.allPosts)
+    const allPosts = useSelector(state => state.post.allPosts);
+    const loading = useSelector(state => state.post.loading);
+
+    if (loading) {
+        return (
+            <View style={styles.center}>
+                <ActivityIndicator color={THEME.MAIN_COLOR}/>
+            </View>
+        );
+    }
 
     return <PostList data={allPosts} onOpen={openPostHandler} />;
 };
@@ -42,3 +52,11 @@ MainScreen.navigationOptions = ({ navigation }) => ({
 });
 
 export default MainScreen;
+
+const styles = StyleSheet.create({
+    center: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,9 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { postReducer } from './reducers/post';
+import thunk from 'redux-thunk';
 
 const roorReducer = combineReducers({
     post: postReducer,
 });
 
-export default createStore(roorReducer);
+export default createStore(roorReducer, applyMiddleware(thunk));
